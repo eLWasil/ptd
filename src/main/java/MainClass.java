a612fb547770525b215832962134eb500bc9b3ed
Łukasz Wasilewski <lukasz.wasilewski0395@gmail.com>
2018-11-30 00:34:24 +0100

    Charts (beta)

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -10,7 +10,7 @@ public class MainClass {
         FrameManager frameManager = new FrameManager("Okienko");
         ChartManager chartManager = new ChartManager();
 
-        Lab1 think = new Lab1(100);
+        Lab1 think = new Lab1(200);
         List nVector = think.getVectorN();
         List xVector = think.getVectorX();
         List zVector = think.getVectorZ();
@@ -22,13 +22,13 @@ public class MainClass {
         List gbVector = think.getVectorGb();
         List gcVector = think.getVectorGc();
 
-        ChartPanel chartPanel = chartManager.makeXYLineChart("Lab 1, zadanie 1", "N", nVector, "Xs", xVector);
+//        ChartPanel chartPanel = chartManager.makeXYLineChart("Lab 1, zadanie 1", "N", nVector, "Xs", xVector);
 //        ChartPanel chartPanel = chartManager.makeXYLineChart("Lab 1, zadanie 2a", "N", nVector, "Xs", zVector);
 //        ChartPanel chartPanel = chartManager.makeXYLineChart("Lab 1, zadanie 2b", "N", nVector, "Xs", vVector);
 //        ChartPanel chartPanel = chartManager.makeXYLineChart("Lab 1, zadanie 3", "N", nVector, "Xs", uVector);
 //        ChartPanel chartPanel = chartManager.makeXYLineChart("Lab 1, zadanie 4a", "N", gTimeVec, "Xs", gaVector);
 //        ChartPanel chartPanel = chartManager.makeXYLineChart("Lab 1, zadanie 4b", "N", gTimeVec, "Xs", gbVector);
-//        ChartPanel chartPanel = chartManager.makeXYLineChart("Lab 1, zadanie 4c", "N", gTimeVec, "Xs", gcVector);
+        ChartPanel chartPanel = chartManager.makeXYLineChart("Lab 1, zadanie 4c", "N", gTimeVec, "Xs", gcVector);
 
         Lab2 lab2 = new Lab2(xVector);
         List alphaVec = lab2.getVectorAlphan();
